fa362a72ac7854af59eb2f874ec2500b8bc3c6a7
Zubair Ahmed <zubair1024@gmail.com>
2020-01-07 22:06:23 +0400

    config change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Thermal Insulation Solutions`,
+    description: `Your Insulation Experts`,
+    author: `@grizzlybit`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
